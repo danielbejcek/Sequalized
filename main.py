1d76578eab5194b4ca1a228013daa04bf9dece98
danielbejcek <daniel.bejcek@seznam.cz>
2023-11-30 12:48:59 +0100

    Updated add_employee constraints

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
 db = mysql.connector.connect(
-    host="",
-    user="",
-    password="",
-    database="")
+    host="localhost",
+    user="Daniel",
+    password="Goldbergerova1",
+    database="EmployeeData")
 mycursor = db.cursor()
 
 class EmployeeDatabase():
@@ -18,10 +18,21 @@ class EmployeeDatabase():
     def add_employee(self):
         add_emp = "INSERT INTO employees (first_name, last_name, age, gender, salary) VALUES (%s,%s,%s,%s,%s)"
         values = (self.firstname, self.lastname, self.age, self.gender, self.salary)
-        mycursor.execute(add_emp, values)
-        db.commit()
-        last_row_id = mycursor.lastrowid
-        return last_row_id, values
+
+        if any(value == "" for value in [self.firstname, self.lastname, self.gender]):
+            raise TypeError("Value cannot be empty")
+
+        if any(char.isdigit() for char in self.firstname + self.lastname + self.gender):
+            raise TypeError("Characters cannot be numbers")
+
+        if (not isinstance(self.age, int) or not isinstance(self.salary, int)) or self.age < 1 or self.salary < 1:
+            raise ValueError("Input must be a number and above zero")
+
+        else:
+            mycursor.execute(add_emp, values)
+            db.commit()
+            last_row_id = mycursor.lastrowid
+            return last_row_id, values
 
     def add_multiple_employees(self, employees):
         insert_emp = "INSERT INTO employees (first_name, last_name, age, gender, salary) VALUES (%s,%s,%s,%s,%s)"
@@ -50,6 +61,17 @@ emp1 = EmployeeDatabase("Daniel","Bejček",29,"M",10000)
 emp2 = EmployeeDatabase("Tereza","Vojtěchová",25,"F",9000)
 emp3 = EmployeeDatabase("John","Doe",30,"M",50000)
 multi_emp = [("Daniel","Bejček",29,"M",10000), ("Tereza","Vojtěchová",25,"F",9000)]
+
+# mycursor.execute("ALTER TABLE employees MODIFY COLUMN emp_id INT default = 1")
+# mycursor.execute("ALTER TABLE employees AUTO_INCREMENT = 1")
+# mycursor.execute("DELETE FROM employees WHERE emp_id BETWEEN 10 AND 20")
+
+
+emp2.add_employee()
+# emp3.remove_employee(109)
+# emp2.remove_multiple_employees(114,122)
+# emp1.add_multiple_employees(multi_emp)
+# emp1.get_employee(27)
 
 
 
--- test_main.py
+++ test_main.py
@@ -4,20 +4,20 @@ from main import EmployeeDatabase
 from unittest.mock import Mock
 
 db = mysql.connector.connect(
-            host="",
-            user="",
-            password="",
-            database="")
+            host="localhost",
+            user="Daniel",
+            password="Goldbergerova1",
+            database="EmployeeData")
 mycursor = db.cursor()
 
 
 class TestEmployeeDatabase(unittest.TestCase):
     def setUp(self):
         self.test_emp1 = EmployeeDatabase("John","Doe",30,"M",50000)
-        self.test_emp2 = EmployeeDatabase("Steve","Jackson",20,"M",50000)
+        self.test_emp2 = EmployeeDatabase("Steve","Jackson",10,"M",10)
         self.test_emp3 = EmployeeDatabase("Daniel","Bejček",29,"M",10000)
         # last_row_id recieved as a return from 'add_employee' to monitor the last added and deleted employee
-        self.last_row_id,self.added_employee = self.test_emp3.add_employee()
+        self.last_row_id,self.added_employee = self.test_emp1.add_employee()
 
     def tearDown(self):
         self.test_emp1.remove_employee(self.last_row_id)
@@ -25,7 +25,7 @@ class TestEmployeeDatabase(unittest.TestCase):
 
     def test_database_employee_insert(self):
         """Check to verify that employee has infact been added to the database."""
-        get_db_employee = self.test_emp2.get_employee(self.last_row_id)
+        get_db_employee = self.test_emp1.get_employee(self.last_row_id)
         # Slice [1:] to exclude the 'emp_id' column and match the result
         self.assertEqual(self.added_employee, get_db_employee[1:], "Incorrect employee")
 
@@ -35,13 +35,13 @@ class TestEmployeeDatabase(unittest.TestCase):
         test_lastname = any(char.isdigit() for char in self.test_emp2.lastname) or self.test_emp2.lastname == ""
         test_gender = any(char.isdigit() for char in self.test_emp2.gender) or self.test_emp2.gender == ""
 
-        """Check to see if age or salary are not a string."""
-        if isinstance(self.test_emp2.age, int) and isinstance(self.test_emp2.salary, int):
+        """Check to see if age or salary are not a string and that both are above 0."""
+        if isinstance(self.test_emp2.age, int) and self.test_emp2.age > 0 and isinstance(self.test_emp2.salary, int) and self.test_emp2.salary > 0:
             has_string = False
         else:
             has_string = True
 
-        self.assertFalse(has_string, "age or salary cannot be string")
+        self.assertFalse(has_string, "age or salary cannot be string and must be above 0")
         self.assertFalse(test_firstname, "firstname cannot be an empty string or contain integers")
         self.assertFalse(test_lastname, "lastname cannot be an empty string or contain integers")
         self.assertFalse(test_gender, "gender cannot be an empty string or contain integers")
