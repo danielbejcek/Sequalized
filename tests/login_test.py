0f2d269edac696ff820831d157bfdc148511d2f1
danielbejcek <daniel.bejcek@seznam.cz>
2023-12-06 15:16:32 +0100

    Page Object Model commit

--- tests/login_test.py
+++ tests/login_test.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 import pytest
+from POMdemo.pages.login_page import LoginPage
 
 
 
@@ -14,10 +15,9 @@ import pytest
 @pytest.fixture()
 def driver():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    # driver.get("https://www.google.com")
     driver.get("https://trytestingthis.netlify.app/")
 
-    """Surpass cookies window"""
+    """Surpass Google cookies window"""
     # cookies_popup = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.ID, "CXQnmb")))
     # accept_all_button = cookies_popup.find_element(By.ID, "L2AGLb").click()
 
@@ -25,18 +25,18 @@ def driver():
     driver.close()
     driver.quit()
 
-@pytest.mark.parametrize("username, password",[
-    ("test","test"),
-    ("user2","pass2"),
-    ("user3","pass3")])
+# @pytest.mark.parametrize("username, password",[
+#     ("test","test"),
+#     ("user2","pass2"),
+#     ("user3","pass3")])
 
-def test_login_input(driver, username, password):
-    user_name_loc = driver.find_element(By.ID, "uname")
-    pwd_loc = driver.find_element(By.ID, "pwd")
-
-    user_name_loc.send_keys(username)
-    pwd_loc.send_keys(password)
-    login_btn = driver.find_element(By.XPATH,"//input[@value='Login']").click()
-    assert "Successful" in driver.page_source
+def test_login(driver):
+    login_page = LoginPage(driver)
+    login_page.open_page("https://trytestingthis.netlify.app/")
+    time.sleep(2)
+    login_page.enter_username("test")
+    login_page.enter_password("test")
+    time.sleep(2)
+    login_page.submit_login()
 
 
