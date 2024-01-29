a7526efd150265691bc31fb57a6485b1545878e2
danielbejcek <daniel.bejcek@seznam.cz>
2024-01-29 12:57:35 +0100

    Cookie clicker

--- POMdemo/pages/login_page.py
+++ POMdemo/pages/login_page.py
@@ -1,23 +1,74 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium import webdriver
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
-class LoginPage:
+class CookieClicker:
     def __init__(self, driver):
         """Defining locators"""
         self.driver = driver
-        self.username_textbox = (By.ID, "uname")
-        self.password = (By.ID, "pwd")
-        self.login_button = (By.XPATH,"//input[@value='Login']")
+        self.language_element = (By.ID,"langSelect-EN")
 
-    def open_page(self, url):
+        self.cookie_element = (By.ID,"bigCookie")
+        self.cookie_counter_element = (By.ID,"cookies")
+        self.cursor_price = (By.ID, "productPrice0")
+        self.grandma_price = (By.ID, "productPrice1")
+        self.purchase_list = []
+        self.actions = ActionChains(driver)
+
+    def open_page(self,url):
         self.driver.get(url)
+    def cookie_manager(self):
+        try:
+            cookies_1 = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "fc-footer-buttons")))
+            accept_all_1 = cookies_1.find_element(By.CLASS_NAME, "fc-primary-button").click()
+        finally:
+            time.sleep(2)
 
-    def enter_username(self, username):
-        self.driver.find_element(*self.username_textbox).send_keys(username)
+    def select_language(self):
+        self.driver.implicitly_wait(3)
+        self.driver.find_element(*self.language_element).click()
 
-    def enter_password(self, password):
-        self.driver.find_element(*self.password).send_keys(password)
+    def close_cookies(self):
+        try:
+            self.driver.implicitly_wait(3)
+            cookies_2 = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "cc_container--open")))
+            accept_all_2 = cookies_2.find_element(By.LINK_TEXT, "Got it!").click()
+            self.driver.implicitly_wait(3)
+            cookies_3 = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "google-revocation-link-placeholder")))
+            accept_all_3 = cookies_3.find_element(By.CSS_SELECTOR, "[aria-label='Close']").click()
+        finally:
+            time.sleep(2)
+    def back_up_icon(self):
+        self.driver.implicitly_wait(1)
+        save_icon = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.ID, "notes")))
+        close_icon = save_icon.find_element(By.CLASS_NAME, "close").click()
 
-    def submit_login(self):
-        self.driver.find_element(*self.login_button).click()
+
+    def action_chain_cookie_click(self):
+        for i in range(100):
+            self.actions.double_click(self.driver.find_element(*self.cookie_element))
+            self.actions.perform()
+            """Locating the text property of the HTML element 'cookies' and striping it of the rest of the text"""
+            count = int(self.driver.find_element(*self.cookie_counter_element).text.split(" ")[0])
+            print(count)
+            cookie_cursor = int(self.driver.find_element(*self.cursor_price).text)
+            if count >= cookie_cursor:
+                self.actions.move_to_element(self.driver.find_element(*self.cursor_price))
+                self.actions.click()
+                self.actions.perform()
+
+
+
+
+
+
+
+
 
 
--- tests/login_test.py
+++ tests/login_test.py
@@ -1,42 +1,34 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 import pytest
-from POMdemo.pages.login_page import LoginPage
-
+from POMdemo.pages.login_page import CookieClicker
 
 
 
 @pytest.fixture()
 def driver():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    driver.get("https://trytestingthis.netlify.app/")
-
-    """Surpass Google cookies window"""
-    # cookies_popup = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.ID, "CXQnmb")))
-    # accept_all_button = cookies_popup.find_element(By.ID, "L2AGLb").click()
-
     yield driver
-    driver.close()
-    driver.quit()
 
 # @pytest.mark.parametrize("username, password",[
 #     ("test","test"),
 #     ("user2","pass2"),
 #     ("user3","pass3")])
 
-def test_login(driver):
-    login_page = LoginPage(driver)
-    login_page.open_page("https://trytestingthis.netlify.app/")
+
+def test_website_2(driver):
+    webpage = CookieClicker(driver)
+    webpage.open_page("https://orteil.dashnet.org/cookieclicker/")
+
+    webpage.cookie_manager()
+    webpage.select_language()
     time.sleep(2)
-    login_page.enter_username("test")
-    login_page.enter_password("test")
+    webpage.close_cookies()
+    time.sleep(1)
+    webpage.back_up_icon()
     time.sleep(2)
-    login_page.submit_login()
+    webpage.action_chain_cookie_click()
 
 
